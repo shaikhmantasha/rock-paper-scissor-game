906e9c1c2fcecc80a3aece5fcbcc3e73ac57bc42
shaikhmantasha <88544998+shaikhmantasha@users.noreply.github.com>
2021-10-22 15:37:34 +0530

    File added

---
+++ Another_game.py
@@ -0,0 +1,29 @@
+# import random
+
+# player = input("choose your action : ")
+# computer_choose = ['rock' , 'paper' , 'scissor']
+# computer_action = random.choice(computer_choose)
+# print(f"you chose : {player} , compuer chose {computer_action} ")
+
+# if computer_action == player:
+#     print(f"you both chose the same thing {player}, this game is a tie")
+# elif player == 'rock':
+#     if computer_action == 'paper':
+#         print("paper covers rock, you win!")
+# else:
+#     print("rock kills scissor , you lose!")
+
+
+# elif player == 'paper':  
+#     if computer_action == 'scissor':
+#         print("scissor cuts paper , you lose")
+# else:
+#     print("paper covers rock, you win!")
+
+
+# elif player == 'scissor':
+#     if computer_action == 'paper':
+#         print("scissor cuts paper, you win")
+# else:
+#     print("rock kills scissor , you lose!")
+
